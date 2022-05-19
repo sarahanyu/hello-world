4db46aeb74d4ea0d272effaaafaf1b9f9e45d09f
sarahanyu <105870007+sarahanyu@users.noreply.github.com>
2022-05-19 22:36:51 +0800

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,19 @@
+"""
+File: hello_world.py
+Name: 
+-----------------------------------
+This program prints 'hello, world!'
+on Console and shows the basic 
+Python syntax
+"""
+
+
+def main():
+	print("Hello world!", end="")
+	print("Favorite move: La la land", end="")
+	print('Fun fact: I speak 3 languages', end="")
+
+### DO NOT EDIT THE CODE BELOW THIS LINE ###
+
+if __name__ == '__main__':
+	main()
